6a1e39e9feadd20f4d8824ae815c91a6d7dbcbc3
Chase Todd Hawkins <sc00px.mask@gmail.com>
2025-10-17 23:23:30 -0400

    Enabled viaIR optimizer to resolve stack depth

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,22 +1,49 @@
-require("dotenv").config();
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 module.exports = {
-  solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: { enabled: true, runs: 200 },
-      viaIR: true
-    }
-  },
+  defaultNetwork: "hardhat",
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      url: process.env.SEPOLIA_URL || "",
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
     },
     empac: {
-      url: process.env.EMPAC_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+      url: process.env.EMPAC_URL || "",
+      accounts: process.env.EMPAC_PRIVATE_KEY ? [process.env.EMPAC_PRIVATE_KEY] : [],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          viaIR: true,
+        },
+      },
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          viaIR: true,
+        },
+      },
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          viaIR: true,
+        },
+      },
+    ],
+  },
 };
